73ba7e9671759c6eb9c75f58f6e253eca9d0f944
James Langridge <jlangridge@users.noreply.github.com>
2018-07-25 16:26:14 +0100

    Initial solution setup

---
+++ src/IronRockstar.Cmd/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IronRockstar.Cmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/IronRockstarCompiler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IronRockstarCompiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
