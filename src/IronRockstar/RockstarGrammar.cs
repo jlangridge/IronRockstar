931fab684bfda35fbb84484427d151324e6ac992
James Langridge <jlangridge@users.noreply.github.com>
2018-07-27 10:40:45 +0100

    Initial Tests - Tests don't run

---
+++ src/IronRockstar/RockstarGrammar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Irony.Ast;
+using Irony.Parsing;
+
+namespace IronRockstar
+{
+    public class RockstarGrammar : Grammar
+    {
+
+        public RockstarGrammar() : base(true) //Rockstar is case sensitive (for variable assignment)
+        {
+            //var comment = new CommentTerminal("comment", "(", ")");
+            //NonGrammarTerminals.Add(comment);
+        }
+    }
+}
---
+++ tests/IronRockstar.Tests/CoreGrammarTests.cs
@@ -0,0 +1,21 @@
+﻿
+using Irony.Parsing;
+
+namespace IronRockstar.Tests
+{
+    using Xunit;
+
+    public class CoreGrammarTests
+    {
+        [Fact]
+        public void BracketedTermsShouldBeTreatedAsComments()
+        {
+            var parser = new Parser(new RockstarGrammar());
+            var tree = parser.Parse("(This is a comment)");
+            Assert.True(tree.Tokens.Count ==1, "Parsed tree contains no tokens");
+            var token = tree.Tokens[0];
+            Assert.Equal(TokenCategory.Comment, token.Category);
+
+        }
+    }
+}
